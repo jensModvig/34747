5e6f1858288101842faf3bd9f90874a57e347754
DanielHolmelund <danielholmelund@hotmail.com>
2023-06-09 15:47:58 +0200

    updates

---
+++ simulation/get_route.m
@@ -0,0 +1,12 @@
+x_scale = 0.6;
+y_scale = 0.52;
+z_scale = 0.73;
+
+x_offset = 0.3;
+y_offset = 0.5;
+z_offset = 0.25;
+
+start_letter = "G";
+end_letter = "B";
+
+route = find_route_3d(wall, start_letter, end_letter, x_scale, y_scale, z_scale, x_offset, y_offset, z_offset);
